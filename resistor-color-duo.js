20655bd1fae21b2ff373c5cfb7e413d1f1e1e367
coderAurora <coderauroratm@gmail.com>
2021-06-11 16:55:47 -0400

    added my solution to resistor-color-duo exercism.io project

---
+++ resistor-color-duo.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const colorList = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+}
+
+export const decodedValue = (colors) => {
+    return parseInt(`${colorList[colors[0]]}${colorList[colors[1]]}`, 10);
+}
+
+decodedValue(["brown", "black"]);
